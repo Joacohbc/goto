6c7f554ebf43a657d732b3d2a75a3b444124d77b
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-06-28 16:26:07 -0300

    Improve the install.sh. Check if already exist other installation of goto
    - Check if there is a ~/.config/goto
    - Check for an alias.sh entry in the shell file

--- alias.sh
+++ alias.sh
@@ -6,12 +6,9 @@ goto() {
     OUTPUT=$("$GOTO_FILE" $@)
 
     #If the return "2", the program return a gpath successfully
-    if [[ "$?" == "2" ]]; then
-
+    if [[ $? -eq 2 ]]; then
         cd "$OUTPUT"   
-
         echo "Go to:" $OUTPUT
-        
     else 
         #If not a "2", can be 0 or 1
         echo "$OUTPUT"
--- install.sh
+++ install.sh
@@ -16,17 +16,16 @@ exito() {
     echo "$(greenprint "$1")"
 }
 
+#Compile the goto.bin if the user wants
 echo "You have go installed?(y or any)"
 read op
 
 if [ "$op" = "y" ]; then
-
-    GOTO_BIN="goto.bin"
-
-    rm -f $GOTO_BIN
+    
+    rm -f goto.bin
 
     #Build the code
-    go build -o $GOTO_BIN ./*.go
+    go build -o goto.bin ./*.go
 
     if [ $? -eq 0 ]; then
         exito "Compaling successfully"
@@ -34,9 +33,6 @@ if [ "$op" = "y" ]; then
         error "Compaling failed"
         exit 1
     fi
-
-else
-    GOTO_BIN="bin/goto.bin"
 fi
 
 #if $XDG_CONFIG_HOME is empty
@@ -48,9 +44,14 @@ fi
 #Define the config dir
 CONFIG_DIR="$XDG_CONFIG_HOME"/goto/
 
+#Check that the config dir doesn't exists
+if [ -e "$CONFIG_DIR" ]; then
+    error "The $HOME/.config/goto/ already exists, delete it to avoid conflicts"
+    exit 1
+fi
+
 #Create the config dir
 mkdir -p $CONFIG_DIR
-
 if [ $? -eq 0 ]; then
     exito "Config dir created successfully"
 else
@@ -58,8 +59,22 @@ else
     exit 1
 fi
 
-#Copy all of the repository to CONFIG_DIR
-cp -r ./* $CONFIG_DIR
+#Copy binary file, alias file and README of the repository to CONFIG_DIR
+cp ./bin/goto.bin $CONFIG_DIR
+
+if [ $? -ne 0 ]; then
+    error "File couldn't be copied"
+    exit 1
+fi
+
+cp ./alias.sh $CONFIG_DIR
+
+if [ $? -ne 0 ]; then
+    error "File couldn't be copied"
+    exit 1
+fi
+
+cp ./README.md $CONFIG_DIR
 
 if [ $? -eq 0 ]; then
     exito "All files copied successfully"
@@ -76,6 +91,14 @@ while true; do
     read SHELL_FILE
 
     if [ -f "$SHELL_FILE" ]; then
+
+
+        cat $SHELL_FILE | grep "source "$CONFIG_DIR"alias.sh"
+        if [ $? -eq 0 ]; then
+            error "You already have \"source "$CONFIG_DIR"alias.sh\" in your shell configuration. Delete to avoid conflict"
+            exit 1
+        fi
+
         #Add the alias.sh to $SHELL_FILE
         echo "" >> $SHELL_FILE #New line
         echo "#Aliases to use goto:" >> $SHELL_FILE
@@ -89,7 +112,8 @@ while true; do
 done 
 
 #Give excute permission to the bin file
-chmod +x ""$CONFIG_DIR"$GOTO_BIN"
+GOTO_BIN_FILE="$CONFIG_DIR"goto.bin
+chmod +x $GOTO_BIN_FILE
 
 if [ $? -eq 0 ]; then
     exito "Permission added successfully"
@@ -102,28 +126,28 @@ fi
 #---------------------------------------#
 
 #Add the GOTO_FILE variable
-aliasFile=""$CONFIG_DIR"alias.sh.new"
+aliasFile="$CONFIG_DIR"alias.sh.new
 
 #Put the advise menssages and GOTO_FILE variable in the alias
-echo "##ADD THIS FILE TO .bashrc OR .zshrc WITH \"SOURCE <ABSOLUTE-PATH-OF-THIS-FILE>\"" > $aliasFile
+echo "##ADD THIS FILE TO .bashrc OR .zshrc WITH \"source <ABSOLUTE-PATH-OF-THIS-FILE>\"" > $aliasFile
 echo "#GOTO_FILE=\"<ABSOLUTE-PATH-OF-THIS-FILE>\"" >> $aliasFile
-echo "GOTO_FILE=\""$CONFIG_DIR"$GOTO_BIN\"" >> $aliasFile
+echo "GOTO_FILE=\"$GOTO_BIN_FILE\"" >> $aliasFile
 echo "" >> $aliasFile
 
-#Number of lines of the text
+#Number of lines of alias.sh 
 num=$(wc -l alias.sh | cut -d " " -f 1)
 
 #Remainder of 3 first lines of orginal text(the lines of GOTO_FILE variable)
 result=$(expr $num - 3)
 
-#Text without 3 first lines
+#Select text from alias.sh without the 3 first lines (placeholders lines)
 tail -n$result ./alias.sh >> $aliasFile
 
 #Delete the current alias.sh of the config file
-rm ""$CONFIG_DIR"alias.sh"
+rm "$CONFIG_DIR"alias.sh
 
 #Change the name of alias.sh.new to alias.sh of the config file
-mv $aliasFile ""$CONFIG_DIR"alias.sh"
+mv $aliasFile "$CONFIG_DIR"alias.sh
 
 #Some advises:
 echo "This almost complete, please restart the terminal and check if all work correctly"
