f2a986732ebdd6c7aebe7886bc07b8a0558d984b
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-07-18 18:03:29 -0300

    Add the search option to find gpaths with path/abbreviation

--- src/cmd/list.go
+++ src/cmd/list.go
@@ -11,47 +11,13 @@ import (
 var listCmd = &cobra.Command{
 	Use:     "list-path",
 	Aliases: []string{"list"},
-	Args:    cobra.ExactArgs(0),
 	Short:   "List goto-paths in the goto-paths file",
-	Example: `
-# To list all goto-paths
-goto list
-
-# To list a specific goto-path you can use the Path or the Abbreviation 
-goto list --path ~/Documents
-goto list --abbv docs
-`,
-
 	Run: func(cmd *cobra.Command, _ []string) {
 
 		//Load the goto-paths file to array
 		gpaths := utils.LoadGPaths(cmd)
 
-		// If the any path flag is passed
-		if utils.CurrentDirFlagPassed(cmd) || utils.PathFlagPassed(cmd) {
-
-			path := utils.GetPath(cmd)
-
-			for i, gpath := range gpaths {
-				if gpath.Path == path {
-					fmt.Printf("%v - %s\n", i, gpath.String())
-					return
-				}
-			}
-			cobra.CheckErr(fmt.Sprintf("the path \"%s\" doesn't exist in the gpaths-file", path))
-
-		} else if utils.AbbvFlagPassed(cmd) { // If the abbreviation flag is passed
-			abbv := utils.GetAbbreviation(cmd)
-			for i, gpath := range gpaths {
-
-				if gpath.Abbreviation == abbv {
-					fmt.Printf("%v - %s\n", i, gpath.String())
-					return
-				}
-			}
-			cobra.CheckErr(fmt.Sprintf("doesn't exist a path with that abbreviation \"%s\"", abbv))
-
-		} else if utils.FlagPassed(cmd, "reverse") { //If the flag "reverse" is passed
+		if utils.FlagPassed(cmd, "reverse") { // If the reverse flag is passed
 			for i := range gpaths {
 				fmt.Printf("%v - %s\n", len(gpaths)-i-1, gpaths[len(gpaths)-i-1].String())
 			}
@@ -70,8 +36,5 @@ func init() {
 	rootCmd.AddCommand(listCmd)
 
 	//Flags
-	listCmd.Flags().StringP(utils.FlagPath, "p", "", "The Path to delete")
-	listCmd.Flags().StringP(utils.FlagAbbreviation, "a", "", "The Abbreviation of the Path")
-	listCmd.Flags().BoolP(utils.FlagCurrentDir, "c", false, "The Path to update will be the current directory (\"path\" parameter will be overwrite)")
 	listCmd.Flags().BoolP("reverse", "R", false, "List the goto-paths in reverse")
 }
---
+++ src/cmd/search.go
@@ -0,0 +1,72 @@
+package cmd
+
+import (
+	"fmt"
+	"goto/src/utils"
+
+	"github.com/spf13/cobra"
+)
+
+// searchGPathCmd represents the searchGPath command
+var searchCmd = &cobra.Command{
+	Use:     "search-path",
+	Aliases: []string{"search", "find-path", "find"},
+	Short:   "search goto-paths in the goto-paths file",
+	Example: `
+# To search a specific goto-path you can use the Path or the Abbreviation 
+goto search --path ~/Documents
+goto search --abbv docs
+`,
+	PreRun: func(cmd *cobra.Command, args []string) {
+
+		//If the number or flags are 0 or more than 2, return an error
+		if cmd.Flags().NFlag() == 0 || cmd.Flags().NFlag() > 2 {
+			cobra.CheckErr("you must specify only one flag to find a gpath (Or Path or Abbreviation)")
+		}
+
+		//If only one flags is passed and it is the temporary flags, return an error
+		if cmd.Flags().NFlag() == 1 && utils.TemporalFlagPassed(cmd) {
+			cobra.CheckErr("you must specify one flag to find a gpath (Or Path or Abbreviation)")
+		}
+	},
+	Run: func(cmd *cobra.Command, _ []string) {
+
+		//Load the goto-paths file to array
+		gpaths := utils.LoadGPaths(cmd)
+
+		// If the any path flag is passed
+		if utils.CurrentDirFlagPassed(cmd) || utils.PathFlagPassed(cmd) {
+
+			path := utils.GetPath(cmd)
+
+			for i, gpath := range gpaths {
+				if gpath.Path == path {
+					fmt.Printf("%v - %s\n", i, gpath.String())
+					return
+				}
+			}
+			cobra.CheckErr(fmt.Errorf("the path \"%s\" doesn't exist in the gpaths-file", path))
+
+		} else {
+			abbv := utils.GetAbbreviation(cmd)
+
+			for i, gpath := range gpaths {
+				if gpath.Abbreviation == abbv {
+					fmt.Printf("%v - %s\n", i, gpath.String())
+					return
+				}
+			}
+			cobra.CheckErr(fmt.Errorf("doesn't exist a path with that abbreviation \"%s\"", abbv))
+		}
+	},
+}
+
+func init() {
+	//Add this command to RootCommand
+	rootCmd.AddCommand(searchCmd)
+
+	//Flags
+	searchCmd.Flags().StringP(utils.FlagPath, "p", "", "The Path to delete")
+	searchCmd.Flags().StringP(utils.FlagAbbreviation, "a", "", "The Abbreviation of the Path")
+	searchCmd.Flags().BoolP(utils.FlagCurrentDir, "c", false, "The Path to update will be the current directory (\"path\" flag value will be overwrite)")
+}
