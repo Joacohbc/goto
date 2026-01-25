37bc0920e2cc97141d1c06cff4687e4e7199b5a6
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2026-01-25 04:50:08 +0000

    feat: add comprehensive tests for delete, search, and update functionalities

--- tests/delete_test.go
+++ tests/delete_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"goto/src/cmd"
 	"goto/src/utils"
+	"os"
 	"testing"
 )
 
@@ -65,4 +66,47 @@ func TestDeleteByIndex(t *testing.T) {
 	if gpaths[0].Abbreviation != "p1" {
 		t.Errorf("Expected remaining path to be 'p1', got '%s'", gpaths[0].Abbreviation)
 	}
+}
+
+func TestDeleteByPath(t *testing.T) {
+	resetTempFile(t)
+	c := getTempCmd()
+
+	cmd.AddCmd.Run(c, []string{".", "p1"})
+
+	delCmd := getTempCmd()
+	delCmd.Flags().StringP(utils.FlagPath, "p", "", "")
+
+	cwd, _ := os.Getwd()
+	delCmd.Flags().Set(utils.FlagPath, cwd)
+
+	captureOutput(func() {
+		cmd.DeleteCmd.Run(delCmd, []string{})
+	})
+
+	gpaths := utils.LoadGPaths(c)
+	// Should delete p1. Default remains.
+	found := false
+	for _, gp := range gpaths {
+		if gp.Abbreviation == "p1" {
+			found = true
+		}
+	}
+	if found {
+		t.Error("Path p1 was not deleted by path")
+	}
+}
+
+func TestDeleteNonExistent(t *testing.T) {
+	if os.Getenv("TEST_DELETE_NON_EXISTENT") == "1" {
+		resetTempFile(t)
+
+		delCmd := getTempCmd()
+		delCmd.Flags().StringP(utils.FlagAbbreviation, "a", "", "")
+		delCmd.Flags().Set(utils.FlagAbbreviation, "non_existent")
+
+		cmd.DeleteCmd.Run(delCmd, []string{})
+		return
+	}
+	RunExpectedExit(t, "TestDeleteNonExistent", "TEST_DELETE_NON_EXISTENT")
 }
--- tests/search_test.go
+++ tests/search_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"goto/src/cmd"
 	"goto/src/utils"
+	"os"
 	"strings"
 	"testing"
 )
@@ -26,4 +27,37 @@ func TestSearchByAbbreviation(t *testing.T) {
 	if !strings.Contains(output, "p1") {
 		t.Errorf("Expected 'p1' in search results, got: %s", output)
 	}
+}
+
+func TestSearchByPath(t *testing.T) {
+	resetTempFile(t)
+	c := getTempCmd()
+	cmd.AddCmd.Run(c, []string{".", "p1"})
+
+	searchCtx := getTempCmd()
+	searchCtx.Flags().StringP(utils.FlagPath, "p", "", "")
+	cwd, _ := os.Getwd()
+	searchCtx.Flags().Set(utils.FlagPath, cwd)
+
+	output := captureOutput(func() {
+		cmd.SearchCmd.Run(searchCtx, []string{})
+	})
+
+	if !strings.Contains(output, "p1") {
+		t.Errorf("Expected 'p1' in search by path result, got: %v", output)
+	}
+}
+
+func TestSearchNotFound(t *testing.T) {
+	if os.Getenv("TEST_SEARCH_NOT_FOUND") == "1" {
+		resetTempFile(t)
+
+		searchCtx := getTempCmd()
+		searchCtx.Flags().StringP(utils.FlagAbbreviation, "a", "", "")
+		searchCtx.Flags().Set(utils.FlagAbbreviation, "nothere")
+
+		cmd.SearchCmd.Run(searchCtx, []string{})
+		return
+	}
+	RunExpectedExit(t, "TestSearchNotFound", "TEST_SEARCH_NOT_FOUND")
 }
